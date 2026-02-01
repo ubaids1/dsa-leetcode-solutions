e324ae6003c6a82b31d7f5c02de2e5d489fa6d94
Ubaid <ubaidsiddiqui720@gmail.com>
2026-02-02 02:14:56 +0530

    Add DFS and BFS approaches for invert binary tree

--- Trees/invert_binary_tree.py
+++ Trees/invert_binary_tree.py
@@ -1,7 +1,7 @@
 # LeetCode 226 - Invert Binary Tree
-# Recursive DFS solution
 
-class Solution:
+# Approach 1: Recursive DFS
+class SolutionRecursive:
     def invertTree(self, root):
         # Step 1: base case
         if root is None:
@@ -15,4 +15,44 @@ class Solution:
         self.invertTree(root.right)
 
         # Step 4: return root
+        return root
+    
+# Approach 2: Iterative DFS using Stack
+class SolutionDFS:
+    def invertTree(self, root):
+        if root is None:
+            return None
+
+        stack = [root]
+
+        while stack:
+            node = stack.pop()
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+
+        return root
+
+# Approach 3: BFS using Queue
+from collections import deque
+
+class SolutionBFS:
+    def invertTree(self, root):
+        if root is None:
+            return None
+
+        queue = deque([root])
+
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+
         return root
